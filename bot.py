06efc2906f4f02b314fb1778e60980fc3e2b4d97
rcosserver <mrand@bk.ru>
2020-07-13 17:27:25 +0300

    добавил время и погоду

--- bot.py
+++ bot.py
@@ -1,13 +1,26 @@
-# -*- coding: utf-8 -*-
-import config
-import telebot
+import datetime
+import requests
+import json
 
-bot = telebot.TeleBot(config.token)
+appid = "1884e6b6d5605fee686cf449ac8b1e54"
+
+today = datetime.datetime.today()
+mount = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
+
+mounth_real = int(today.strftime("%m"))
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                   params={'id': 524901, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print('Привет, сегодня', today.strftime("%d"),mount[mounth_real % 12],today.strftime("%Y"),'г.')
+
+#---------------------#
+
+print('Погода в Москве хорошая,', data['weather'][0]['description'],',',data['main']['temp'], 'градуса Цельсия')
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'HI')
 
 
-bot.polling()
+
+
---
+++ pogoda.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+api_key = "1884e6b6d5605fee686cf449ac8b1e54"
+lat = "48.208176"
+lon = "16.373819"
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+
+response = requests.get(url)
+data = json.loads(response.text)
+print(data)
