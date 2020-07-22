edfc3cade1181ce5c50103b0781e9daae9f1347c
rcosserver <61562914+rcosserver@users.noreply.github.com>
2020-07-22 11:08:26 +0300

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_weather(lat, lon):
-    return requests.get(f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=1884e6b6d5605fee686cf449ac8b1e54').json()
-
-print(get_weather(96.95, 21.83))
