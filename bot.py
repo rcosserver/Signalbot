e9372b1b4fdf5c653e5366e14360133939e16fa8
rcosserver <61562914+rcosserver@users.noreply.github.com>
2020-07-22 11:03:18 +0300

    пробую новую реализцию погоды

--- bot.py
+++ bot.py
@@ -14,11 +14,20 @@ today = datetime.datetime.today()
 mount = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
 mounth_real = int(today.strftime("%m"))
 
+
+
 #Погода в Москве
-temp = pyowm.OWM(tempID, {'language': 'ru'})
+#temp = pyowm.OWM(tempID, {'language': 'ru'})
+#observation = temp.weather_at_place('Moscow')
+#w = observation.weather
+#temp = w.get_temperature('celsius')['temp']
+
+temp = owm.weather_manager(tempID, {'language': 'ru'})
 observation = temp.weather_at_place('Moscow')
 w = observation.weather
 temp = w.get_temperature('celsius')['temp']
+
+
 
 text1 = ('Привет, сегодня ' + today.strftime("%d") + ' ' + mount[mounth_real % 12] + ' ' + today.strftime("%Y") + ' г.')
 text2 = ('Погода в Москве хорошая, ' + w.get_detailed_status()+ ' ' + str(temp) + ' °C')
