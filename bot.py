16e10cb84232b9d9c80dafd4d0be9761bc902764
rcosserver <61562914+rcosserver@users.noreply.github.com>
2020-07-23 14:13:11 +0300

    вернул старую погоду

--- bot.py
+++ bot.py
@@ -17,22 +17,14 @@ mounth_real = int(today.strftime("%m"))
 
 
 #Погода в Москве
-#temp = pyowm.OWM(tempID, {'language': 'ru'})
-#observation = temp.weather_at_place('Moscow')
-#w = observation.weather
-#temp = w.get_temperature('celsius')['temp']
-
-owm = OWM('1884e6b6d5605fee686cf449ac8b1e54')
-temp = owm.weather_manager({'language': 'ru'})
+temp = pyowm.OWM(tempID, {'language': 'ru'})
 observation = temp.weather_at_place('Moscow')
 w = observation.weather
 temp = w.get_temperature('celsius')['temp']
 
 
-
 text1 = ('Привет, сегодня ' + today.strftime("%d") + ' ' + mount[mounth_real % 12] + ' ' + today.strftime("%Y") + ' г.')
 text2 = ('Погода в Москве хорошая, ' + w.get_detailed_status()+ ' ' + str(temp) + ' °C')
-
 
 
 @bot.message_handler(content_types=["text"])
