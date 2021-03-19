374b89abed3e71bedc39878755dacc0bcfb283da
rcosserver <61562914+rcosserver@users.noreply.github.com>
2021-03-19 18:08:01 +0300

    Update bot.py
    сделал погоду по новому

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import pyowm
 import config
 import datetime
 import telebot
-import pyowm
+import time
 
-tempID = config.appid
+from pyowm import OWM
+owm = pyowm.OWM('1884e6b6d5605fee686cf449ac8b1e54', language= 'RU')
 bot = telebot.TeleBot(config.token)
 
 
@@ -18,24 +20,27 @@ mounth_real = int(today.strftime("%m"))
 
 #Погода в Москве
 #temp = pyowm.OWM(tempID, {'language': 'ru'})
-#observation = owm.weather_at_place('Москва')
+#observation = temp.weather_at_place('Москва')
 #w = observation.get_weather()
 #temp = w.get_temperature('celsius')['temp']
 
+observation = owm.weather_at_place('Москва')
+w = observation.get_weather()
+temp = w.get_temperature('celsius')['temp']
+
 
 text1 = ('Привет, сегодня ' + today.strftime("%d") + ' ' + mount[mounth_real % 12] + ' ' + today.strftime("%Y") + ' г.')
-#text2 = ('Погода в Москве хорошая, ' + w.get_detailed_status()+ ' ' + str(temp) + ' °C')
+text2 = ('Погода в Москве хорошая, ' + w.get_detailed_status()+ ' ' + str(temp) + ' °C')
 
 
 @bot.message_handler(content_types=["text"])
 def message(message):
     bot.send_message(message.chat.id, text1)
-    #bot.send_message(message.chat.id, text2)
+    bot.send_message(message.chat.id, text2)
 
 
 
 bot.polling(none_stop=True, interval=0)
-
 
 
 
