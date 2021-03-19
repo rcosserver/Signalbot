be6582344b8fef64786d3fae88a78f4f911c4384
rcosserver <61562914+rcosserver@users.noreply.github.com>
2021-03-19 17:54:47 +0300

    Update bot.py
    закоменториорвал погоду

--- bot.py
+++ bot.py
@@ -17,20 +17,20 @@ mounth_real = int(today.strftime("%m"))
 
 
 #Погода в Москве
-temp = pyowm.OWM(tempID, {'language': 'ru'})
-observation = owm.weather_at_place('Москва')
-w = observation.get_weather()
-temp = w.get_temperature('celsius')['temp']
+#temp = pyowm.OWM(tempID, {'language': 'ru'})
+#observation = owm.weather_at_place('Москва')
+#w = observation.get_weather()
+#temp = w.get_temperature('celsius')['temp']
 
 
 text1 = ('Привет, сегодня ' + today.strftime("%d") + ' ' + mount[mounth_real % 12] + ' ' + today.strftime("%Y") + ' г.')
-text2 = ('Погода в Москве хорошая, ' + w.get_detailed_status()+ ' ' + str(temp) + ' °C')
+#text2 = ('Погода в Москве хорошая, ' + w.get_detailed_status()+ ' ' + str(temp) + ' °C')
 
 
 @bot.message_handler(content_types=["text"])
 def message(message):
     bot.send_message(message.chat.id, text1)
-    bot.send_message(message.chat.id, text2)
+    #bot.send_message(message.chat.id, text2)
 
 
 
