413c8913696ebc3f2510d7afab1bf2af0b255b47
rcosserver <61562914+rcosserver@users.noreply.github.com>
2021-03-23 16:54:27 +0300

    Update bot.py
    добавил дни недели

--- bot.py
+++ bot.py
@@ -16,20 +16,30 @@ today = datetime.datetime.today()
 mount = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
 mounth_real = int(today.strftime("%m"))
 
+#день и месяц текстом
+day = today.strftime("%A")
+if day == 'Monday':
+    day = 'понедельник'
+elif day == 'Tuesday':
+    day = "вторник"
+elif day == 'Wednesday':
+    day = "среда"
+elif day == 'Thursday':
+    day = "четверг"
+elif day == 'Friday':
+    day = "пятница"
+elif day == 'Saturday':
+    day = "суббота"
+elif day == 'Sunday':
+    day = "Воскресенье"
 
-
-#Погода в Москве
-#temp = pyowm.OWM(tempID, {'language': 'ru'})
-#observation = temp.weather_at_place('Москва')
-#w = observation.get_weather()
-#temp = w.get_temperature('celsius')['temp']
 
 observation = owm.weather_at_place('Москва')
 w = observation.get_weather()
 temp = w.get_temperature('celsius')['temp']
 
 
-text1 = ('Привет, сегодня ' + today.strftime("%d") + ' ' + mount[mounth_real % 12] + ' ' + today.strftime("%Y") + ' г.')
+text1 = ('Привет, сегодня '+ day + ' ,' + today.strftime("%d") + ' ' + mount[mounth_real % 12] + ' ' + today.strftime("%Y") + ' г.')
 text2 = ('Погода в Москве хорошая, ' + w.get_detailed_status()+ ' ' + str(temp) + ' °C')
 
 
